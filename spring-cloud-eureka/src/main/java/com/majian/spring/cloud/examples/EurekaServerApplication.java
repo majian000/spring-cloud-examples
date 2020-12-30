9fbafba628a412878e1e8a82b0cbf72d56e13d2c
majian <mmdrss@163.com>
2020-12-30 17:24:13 +0800

    移动配置中心放到项目中

--- spring-cloud-eureka/src/main/java/com/majian/spring/cloud/examples/EurekaServerApplication.java
+++ spring-cloud-eureka/src/main/java/com/majian/spring/cloud/examples/EurekaServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class Application {
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
